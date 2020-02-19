b85e926914d16dd889a61ee8bb61503f1ad69a9c
adancas2020 <60270681+adancas2020@users.noreply.github.com>
2020-02-19 07:33:05 -0600

    Add files via upload

---
+++ Practica_03/include/Rectangulo.h
@@ -0,0 +1,54 @@
+#ifndef RECTANGULO_H
+#define RECTANGULO_H
+#include<iostream>
+#include<math.h>
+#include<stdlib.h>
+#include"punto.h"
+using namespace std;
+class Rectangulo
+{
+     private:
+        Punto SuperiorIzq;
+        Punto SuperiorDer;
+        Punto InferiorIzq;
+        Punto InferiorDer;
+    public:
+        Rectangulo()
+        {
+            cout<<"Se ha creado un rectangulo"<<endl;
+        }
+        void AsignarSupIzq(float x,float y)
+        {
+            SuperiorIzq.asignarX(x);
+            SuperiorIzq.asignarY(y);
+        }
+        void AsignarSupDer(float x,float y)
+        {
+            SuperiorDer.asignarX(x);
+            SuperiorDer.asignarY(y);
+        }
+        void AsignarInfIzq(float x,float y)
+        {
+            InferiorIzq.asignarX(x);
+            InferiorIzq.asignarY(y);
+        }
+        void AsignarInfDer(float x,float y)
+        {
+            InferiorDer.asignarX(x);
+            InferiorDer.asignarY(y);
+        }
+        float Perimetro()
+        {
+            float base=abs(InferiorDer.ObtenerX()-InferiorIzq.ObtenerX());
+            float altura=abs(InferiorDer.ObtenerY()-SuperiorIzq.ObtenerY());
+            return (base*2)+(altura*2);
+        }
+        float Area()
+        {
+            float base=abs(InferiorDer.ObtenerX()-InferiorIzq.ObtenerX());
+            float altura=abs(InferiorDer.ObtenerY()-SuperiorIzq.ObtenerY());
+            return (base*altura);
+        }
+};
+
+#endif
---
+++ Practica_03/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "Punto.h"
+#include "Rectangulo.h"
+using namespace std;
+
+int main()
+{
+    Rectangulo miRectangulo;
+    miRectangulo.AsignarSupIzq(1,5);
+    miRectangulo.AsignarSupDer(7,5);
+    miRectangulo.AsignarInfIzq(1,1);
+    miRectangulo.AsignarInfDer(7,1);
+    cout<<"El perimetro del rectangulo es: "<< miRectangulo.Perimetro() <<endl;
+    cout<<"El area del rectangulo es: "<<miRectangulo.Area()<<endl;
+    return 0;
+}
